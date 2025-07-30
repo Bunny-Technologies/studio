6ffa15cc38e91be6adc68727cc5d49eec6aba84b
unknown <dheeraj.thirunahari@externals.adidas.com>
2025-07-30 12:40:18 +0530

    push

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const withNextIntl = createWithNextIntl();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
--- next.config.ts
+++
@@ -1,9 +0,0 @@
-// This file is intentionally left blank. It will be replaced by next.config.mjs
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // this tells Next.js to output a fully static site
-  output: 'export',
-}
-module.exports = nextConfig
-
-//export default nextConfig
