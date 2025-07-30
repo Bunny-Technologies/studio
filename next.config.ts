d85dd70b6e52417b6416b1ba16f8d44de40bff87
unknown <dheeraj.thirunahari@externals.adidas.com>
2025-07-30 10:39:31 +0530

    git runs

--- next.config.ts
+++ next.config.ts
@@ -1,1 +1,9 @@
 // This file is intentionally left blank. It will be replaced by next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // this tells Next.js to output a fully static site
+  output: 'export',
+}
+module.exports = nextConfig
+
+//export default nextConfig
