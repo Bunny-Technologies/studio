7daab24b9875be827e737420007c39c6cc6b19e0
VADALI TEJASVIRAM <vtejasviram@gmail.com>
2025-07-30 09:10:15 +0000

    I am deploying it on someother system for demo but I am getting the foll

--- src/app/login/page.tsx
+++
@@ -1,5 +0,0 @@
-// This file is no longer used and has been moved to /src/app/[locale]/login/page.tsx
-// The middleware now handles redirection from /login to the localized version.
-export default function DeprecatedLoginPage() {
-  return null;
-}
